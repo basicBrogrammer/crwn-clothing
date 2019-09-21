9ff9c756ff3f8ea24a7b7942e6ecb1d2bc493151
Jeremy Ward <jeremy.ward@mckesson.com>
2019-09-21 10:14:31 -0400

    Authentication page with CustomButton and FormInput components

---
+++ src/pages/authentication/authentication.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const Authentication = () => (
+  <div>
+    <SignIn />
+  </div>
+);
+
+export default Authentication;
